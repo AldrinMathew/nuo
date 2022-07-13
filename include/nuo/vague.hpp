8d7d6b1df2d308382110d824f329612d3de25abe
Aldrin Mathew <admin@aldrinmathew.com>
2022-07-13 15:42:41 +0530

    `Vague` fixed
    - The logic of Vague has been fixed and Problem now have copy and move
      semantics

--- include/nuo/vague.hpp
+++ include/nuo/vague.hpp
@@ -7,87 +7,64 @@
 
 namespace nuo {
 
-/**
- * @brief A Problem occured during logic
- *
- */
+// A Problem occured during logic
 class Problem {
 private:
-  /**
-   * @brief Value of the problem
-   *
-   */
+  // Value of the problem
   std::string value;
 
 public:
-  /**
-   * @brief Create a Problem value
-   *
-   * @param _val Value of the problem
-   */
+  // Create a Problem value
   Problem(std::string _val) : value(_val) {}
 
-  /**
-   * @brief Get the problem described by this instance
-   *
-   * @return std::string Value of the problem
-   */
+  // Copy constructor
+  Problem(const Problem &other) : value(other.value) {}
+
+  // Move constructor
+  Problem(Problem &&other) : value(other.value) {}
+
+  // Copy assignment
+  void operator=(const Problem &other) { value = other.value; }
+
+  // Move assignment
+  void operator=(Problem &&other) { value = other.value; }
+
+  // Get the problem described by this instance
   std::string get() { return value; }
 };
 
+// Vague is used when there is a possibility for a useful value, but also for a
+// problem
 template <typename T> class Vague {
 private:
-  /**
-   * @brief An optional value
-   *
-   */
+  // An optional value
   Maybe<T> value;
 
-  /**
-   * @brief An optional problem
-   *
-   */
+  // An optional problem
   Maybe<Problem> problem;
 
 public:
-  Vague() noexcept : value(Null()), problem(Null()) {}
+  Vague() : value(), problem() {}
 
   /**
    * @brief Create a Vague instance with a valid value of the associated type
    *
    * @param value Value of the associated type
    */
-  Vague(T _value) noexcept : value(_value), problem(Null()) {}
+  Vague(T _value) : value(_value), problem() {}
 
-  /**
-   * @brief Create a Vague instance with a Problem
-   *
-   * @param value An instance of Problem
-   */
-  Vague(Problem _problem) noexcept : value(Null()), problem(_problem) {}
+  // Create a Vague instance with a Problem
+  Vague(Problem _problem) : value(), problem(_problem) {}
 
-  /**
-   * @brief Copy constructor for Vague
-   *
-   * @param other Reference to the other Vague instance (lvalue)
-   */
-  Vague(const Vague &other) noexcept
-      : value(other.value), problem(other.problem) {}
+  // Copy constructor for Vague
+  Vague(const Vague &other) : value(other.value), problem(other.problem) {}
 
-  /**
-   * @brief Move constructor for Vague
-   *
-   * @param other Reference to the other Vague instance (rvalue)
-   */
-  Vague(Vague &&other) noexcept : value(other.value), problem(other.problem) {
+  // Move constructor for Vague
+  Vague(Vague &&other) : value(other.value), problem(other.problem) {
     other.value = Null();
     other.problem = Null();
   }
 
-  /**
-   * @brief Destructor for Vague
-   *
-   */
   ~Vague() noexcept {
     if (has()) {
       value.get().~T();
@@ -99,53 +76,27 @@ public:
     }
   }
 
-  /**
-   * @brief Assign reference to another const Vague instance
-   *
-   * @param other
-   */
-  void operator=(const Vague<T> &other) noexcept {
+  // Assign reference to another const Vague instance
+  void operator=(const Vague<T> &other) {
     value = other.value;
     problem = other.problem;
   }
 
-  /**
-   * @brief Assign another value of the associated type
-   *
-   * @param other_val Value of the associated type
-   */
-  void operator=(const T other_val) noexcept { value = other_val; }
+  // Assign another value of the associated type
+  void operator=(const T other_val) { value = other_val; }
 
-  /**
-   * @brief Assign another problem
-   *
-   * @param other_val A Problem value
-   */
-  void operator=(const Problem other_val) noexcept { problem = other_val; }
+  // Assign another problem
+  void operator=(const Problem other_val) { problem = other_val; }
 
-  /**
-   * @brief Whether there is proper value of the associated type stored in this
-   * instance
-   *
-   * @return true If there is no problem
-   * @return false If there is a problem
-   */
+  // Whether there is proper value of the associated type stored in this
+  // instance
   bool has() const noexcept { return value.has(); }
 
-  /**
-   * @brief Whether there is a problem in this instance
-   *
-   * @return true If there is a problem
-   * @return false If there is no problem
-   */
+  // @brief Whether there is a problem in this instance
   bool hasProblem() const noexcept { return problem.has(); }
 
-  /**
-   * @brief Get the value stored in this instance having the associated type.
-   * This function throws NullAccessException when there is no value
-   *
-   * @return T The value with the associated type in this instance
-   */
+  // Get the value stored in this instance having the associated type.
+  // This function throws NullAccessException when there is no value
   T solve(T (*fn)(Maybe<T>, Maybe<Problem>)) const noexcept {
     if (!hasProblem() && has()) {
       return value.get();
@@ -154,27 +105,14 @@ public:
     }
   }
 
-  /**
-   * @brief Ignore any problem in this instance and get the value directly
-   *
-   * @return Maybe<T> The value present
-   */
+  // Ignore any problem in this instance and get the value directly
   Maybe<T> ignore() const noexcept { return value; }
 
-  /**
-   * @brief If there is a proper value in this instance of the associated type,
-   * that will be returned. Otherwise, the provided value is used
-   *
-   * @param other
-   * @return T
-   */
+  // If there is a proper value in this instance of the associated type,
+  // that will be returned. Otherwise, the provided value is used
   T getOr(T other) const noexcept { return value.getOr(other); }
 
-  /**
-   * @brief Get the optional Problem if there is any
-   *
-   * @return Maybe<Problem> The optional problem
-   */
+  // Get the optional Problem if there is any
   Maybe<Problem> getProblem() const noexcept { return problem; }
 };
 
